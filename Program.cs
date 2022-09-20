1b70417654093f407b06558de90e420ff5cf4e90
Nataliya <Natalia.konyaeva@yandex.ru>
2022-09-20 23:20:33 +0300

    Initisl commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+Console.Write ("Введите A: ");
+int a = int.Parse (Console.ReadLine ()!); // 12345
+
+
+Console.Write ("Введите B: ");
+int b = int.Parse (Console.ReadLine ()!);
+
+int multy = a;
+
+for (int i = 1; i < b; i ++) 
+  multy = multy*a;
+
+ Console.WriteLine($"Число {a} в степени {b} равно {multy}");
+
+ 
+
+
+
